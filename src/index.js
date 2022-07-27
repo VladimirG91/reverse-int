70c7a2b111fac7cdce6cf36fb5e386429df3e9a1
VladimirG91 <vladimir.glazkov91@gmail.com>
2022-07-27 20:58:18 +0600

    feat: done this task =)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n < 0 ? (n = n * -1) : (n = n);
+    let str = n.toString();
+    let result = Array.from(str).reverse().join("");
+    return +result;
+};
